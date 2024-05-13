d0553b098e38a62732b468d1d5c1c1c67cbd6406
surya_chilukuri <tejadst202@gmail.com>
2024-05-13 12:08:48 +0200

    before modiying DAP_config.h to match Pico

--- build/CMakeFiles/debugprobe.dir/home/teja/Pico/pico-sdk/lib/tinyusb/src/class/dfu/dfu_device.c.obj.d
+++
@@ -1,6 +0,0 @@
-CMakeFiles/debugprobe.dir/home/teja/Pico/pico-sdk/lib/tinyusb/src/class/dfu/dfu_device.c.obj: \
- /home/teja/Pico/pico-sdk/lib/tinyusb/src/class/dfu/dfu_device.c \
- /home/teja/Pico/pico-sdk/lib/tinyusb/src/tusb_option.h \
- /home/teja/Pico/pico-sdk/lib/tinyusb/src/common/tusb_compiler.h \
- /home/teja/Pico/JTAGprobe/src/tusb_config.h \
- /home/teja/Pico/pico-sdk/lib/tinyusb/src/common/tusb_mcu.h
