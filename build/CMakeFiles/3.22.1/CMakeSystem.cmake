9d1ea4877e5ab84b61873dbcd583d61952645d8c
surya_chilukuri <tejadst202@gmail.com>
2024-05-16 12:44:41 +0200

    Midday Commit

--- build/CMakeFiles/3.22.1/CMakeSystem.cmake
+++ build/CMakeFiles/3.22.1/CMakeSystem.cmake
@@ -1,6 +1,6 @@
-set(CMAKE_HOST_SYSTEM "Linux-6.5.0-28-generic")
+set(CMAKE_HOST_SYSTEM "Linux-6.5.0-35-generic")
 set(CMAKE_HOST_SYSTEM_NAME "Linux")
-set(CMAKE_HOST_SYSTEM_VERSION "6.5.0-28-generic")
+set(CMAKE_HOST_SYSTEM_VERSION "6.5.0-35-generic")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
 include("/home/teja/Pico/pico-sdk/cmake/preload/toolchains/pico_arm_gcc.cmake")
