14717d7791f3855c3e9ca2981e81c85c37b949a3
surya_chilukuri <suryatejach202@gmail.com>
2024-07-31 14:35:00 +0200

    JTAGprobe with pins adapted as per HYDRAprobe

--- include/DAP.h
+++ include/DAP.h
@@ -140,12 +140,12 @@
 
 
 // DAP SWJ Pins
-#define DAP_SWJ_SWCLK_TCK               2       // SWCLK/TCK
-#define DAP_SWJ_SWDIO_TMS               3       // SWDIO/TMS
-#define DAP_SWJ_TDI                     4       // TDI
-#define DAP_SWJ_TDO                     5       // TDO
-#define DAP_SWJ_nTRST                   6       // nTRST
-#define DAP_SWJ_nRESET                  7       // nRESET
+#define DAP_SWJ_SWCLK_TCK               19       // SWCLK/TCK
+#define DAP_SWJ_SWDIO_TMS               14       // SWDIO/TMS
+#define DAP_SWJ_TDI                     18       // TDI
+#define DAP_SWJ_TDO                     21       // TDO
+#define DAP_SWJ_nTRST                   15       // nTRST
+#define DAP_SWJ_nRESET                  16       // nRESET
 
 
 
